58f2616606452790ab594215c55794f33599740a
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-06 08:54:03 +0900

    solve: 10871 X보다 작은 수

--- 0x02/solutions/10871.py
+++ 0x02/solutions/10871.py
@@ -2,3 +2,10 @@
 # https://www.acmicpc.net/problem/10871
 import sys
 input = sys.stdin.readline
+
+n, x = map(int, input().split())
+num_list = list(map(int, input().split()))
+
+for i in range(n):
+    if num_list[i] < x:
+        print(num_list[i], end=' ')
