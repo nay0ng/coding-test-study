6d46bb97d875d149596aea60d8e26e1fe64a0130
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-06 01:21:30 +0900

    solve: 10869 사칙연산

--- 0x02/solutions/10869.py
+++ 0x02/solutions/10869.py
@@ -2,3 +2,10 @@
 # https://www.acmicpc.net/problem/10869
 import sys
 input = sys.stdin.readline
+
+a, b = input().split()
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)//int(b))
+print(int(a)%int(b))
