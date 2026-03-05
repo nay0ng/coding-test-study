3cc57d56fdce3da7abc61e423ecd734d0dfe6f6f
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-06 08:49:58 +0900

    solve: 9498 시험 성적

--- 0x02/solutions/9498.py
+++ 0x02/solutions/9498.py
@@ -2,3 +2,21 @@
 # https://www.acmicpc.net/problem/9498
 import sys
 input = sys.stdin.readline
+
+# Authored by : nayoung918
+# https://www.acmicpc.net/problem/9498
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n > 89:
+    print('A')
+elif n > 79:
+    print('B')
+elif n > 69:
+    print('C')
+elif n > 59:
+    print('D')
+else:
+    print('F')
