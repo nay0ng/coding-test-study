b9b3ba2b89c7ed514307eb95f12b500fa87ff632
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-17 17:24:25 +0900

    solve: 1000 A+B

--- 0x02/solutions/1000.py
+++ 0x02/solutions/1000.py
@@ -1,7 +1,13 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/1000
 import sys
-input = sys.stdin.readline
+# input = sys.stdin.readline
+
+# def input():
+#     return sys.stdin.readline().rstrip()
+
+# 혹은 lambda로 더 간단하게 쓸 수도 있음
+input = lambda: sys.stdin.readline().rstrip()
 
 A, B = map(int, input().split())
 print(A + B)
