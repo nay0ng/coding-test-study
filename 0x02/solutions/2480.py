194c39462080c0a3c631037aece14ce197d914b3
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-17 21:57:15 +0900

    solve: 2480 주사위 세개

--- 0x02/solutions/2480.py
+++ 0x02/solutions/2480.py
@@ -2,3 +2,16 @@
 # https://www.acmicpc.net/problem/2480
 import sys
 input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+set_dice = set([a, b, c])
+
+if len(set_dice) == 1:
+    print(10000 + a*1000)
+
+elif len(set_dice) == 2:
+    for i in set_dice:
+        if [a, b, c].count(i) == 2:
+            print(1000 + i*100)
+else:
+    print(max([a, b, c]) * 100) 
