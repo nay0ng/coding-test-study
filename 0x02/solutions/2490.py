976cb2f1b13c939291e74b17f50c12eaf97bbe9b
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-18 23:16:29 +0900

    solve: 2490 윷놀이

--- 0x02/solutions/2490.py
+++ 0x02/solutions/2490.py
@@ -1,4 +1,10 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2490
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+result_list = ['D', 'C', 'B', 'A', 'E']
+
+for _ in range(3):
+    t = sum(list(map(int, input().split())))
+    print(result_list[t])
