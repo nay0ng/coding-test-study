518b50e4b2b8eeb9783dca9ce4e49f124beb63dc
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 15:12:05 +0900

    solve: 2439 별 찍기 - 2

--- 0x02/solutions/2439.py
+++ 0x02/solutions/2439.py
@@ -8,3 +8,20 @@ N = int(input())
 print(*[' ' * (N-i) + '*' * i for i in range(1, N+1)], sep='\n')
 
 # sys.stdout.write('\n'.join([' '*N-i + '*'*i for i in range(1, N+1)]))
+
+# # Version 1 - 공백 + 별
+
+# n = int(input())
+# print(*[" " * (n - i) + "*" * i for i in range(1, n + 1)], sep="\n")
+
+
+# # Version 2 - f-string 정렬
+
+# n = int(input())
+# print(*[f"{'*' * i:>{n}}" for i in range(1, n + 1)], sep="\n")
+
+
+# # Version 3 - rjust
+
+# n = int(input())
+# print(*[("*" * i).rjust(n) for i in range(1, n + 1)], sep="\n")
