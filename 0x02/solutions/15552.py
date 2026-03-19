b6b2b52a5287e044df4d17112147a3dacdf5e8dd
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 11:52:18 +0900

    solve: 15552 빠른 A+B

--- 0x02/solutions/15552.py
+++ 0x02/solutions/15552.py
@@ -1,4 +1,9 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/15552
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+
+for _ in range(N):
+    print(sum(list(map(int, input().split()))))
