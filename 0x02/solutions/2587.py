5b6401f2e8136c2d65899e52026d9e14711c4224
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 10:23:48 +0900

    solve: 2587 대표값2

--- 0x02/solutions/2587.py
+++ 0x02/solutions/2587.py
@@ -1,5 +1,11 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2587
 import sys
+import statistics
 input = lambda: sys.stdin.readline().rstrip()
 
+num_list = [int(input()) for _ in range(5)]
+
+print(statistics.mean(num_list))
+# print(int(sum(num_list) // 5))
+print(sorted(num_list)[2])
