11a9344b100d0708797957e71d2c70642d903c82
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 10:50:06 +0900

    10093 숫자

--- 0x02/solutions/10093.py
+++ 0x02/solutions/10093.py
@@ -1,4 +1,11 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/10093
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+a, b = map(int, input().split())
+if b > a:
+    a, b = b, a
+
+print(max(0, a-b-1))
+print(*list(range(b+1, a)), end=' ')
