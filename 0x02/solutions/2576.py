080b9fcaa57fdc68bf4632550646bfa0d64b171b
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 09:24:34 +0900

    solve: 2576 홀수

--- 0x02/solutions/2576.py
+++ 0x02/solutions/2576.py
@@ -1,4 +1,20 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2576
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+total_odd = 0
+min_odd = 100
+
+for _ in range(7):
+    n = int(input())
+    if n % 2 == 1:
+        total_odd += n
+        if min_odd > n:
+            min_odd = n
+    
+if total_odd == 0:
+    print(-1)
+else:
+    print(total_odd)
+    print(min_odd)
