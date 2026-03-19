9b37345609f3dc5d5d666ade0c621330799d9ede
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 16:22:20 +0900

    solve: 2441 별찍기

--- 0x02/solutions/2441.py
+++ 0x02/solutions/2441.py
@@ -1,4 +1,15 @@
 # Authored by : nayoung918
-# https://www.acmicpc.net/problem/2441
+# https://www.acmicpc.net/problem/2442
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+# version 1. 공백 문자 사용
+print(*[' ' * (n-i) + '*'*(i) for i in range(n, 0, -1)], sep='\n')
+
+# # version 2. rjust() 함수 사용
+# print(*[('*' * i).rjust(n) for i in range(n, 0, -1)], sep='\n')
+
+# # version 3. f-string 사용
+# print(*[f"{'*'*i:>{n}}" for i in range(n, 0, -1)], sep='\n')
