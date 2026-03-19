4e1e8027ce9aeee1934a7b649ffddc731e4a1c85
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 15:36:32 +0900

    solve: 2440 별 찍기 - 3

--- 0x02/solutions/2440.py
+++ 0x02/solutions/2440.py
@@ -1,4 +1,8 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2440
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline().rstrip()
+
+n = int(input())
+
+print(*['*'*i for i in range(n, 0, -1)], sep='\n')
