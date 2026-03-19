662969c3d8609f4fe49985b52dab20858d37f0a9
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 12:17:06 +0900

    solve: 2438 별 찍기 - 1

--- 0x02/solutions/2438.py
+++ 0x02/solutions/2438.py
@@ -2,3 +2,14 @@
 # https://www.acmicpc.net/problem/2438
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+
+# for i in range(1, N+1):
+#     sys.stdout.write('*'*i+'\n')
+
+# # 방법 1 - sys.stdout.write + join
+# sys.stdout.write("\n".join("*" * i for i in range(1, N + 1)))
+
+# 방법 2 - print + * 언패킹
+print(*["*" * i for i in range(1, N + 1)], sep="\n")
