ae1faa6d4f4baa2454aa75faced4c40111f94ff9
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 15:10:53 +0900

    solve: 2440 별 찍기 - 3

--- 0x02/solutions/2439.py
+++ 0x02/solutions/2439.py
@@ -2,3 +2,9 @@
 # https://www.acmicpc.net/problem/2439
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+
+print(*[' ' * (N-i) + '*' * i for i in range(1, N+1)], sep='\n')
+
+# sys.stdout.write('\n'.join([' '*N-i + '*'*i for i in range(1, N+1)]))
