8ad4cbf23df8aa479008e44ee96b24efec05debe
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-19 17:06:19 +0900

    solve: 2309 일곱 난쟁이

--- 0x02/solutions/2309.py
+++ 0x02/solutions/2309.py
@@ -1,4 +1,14 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2309
 import sys
-input = sys.stdin.readline
+import itertools
+input = lambda: sys.stdin.readline().rstrip()
+
+input_list = [int(input()) for _ in range(9)]
+
+nCr = itertools.combinations(input_list, 7)
+
+for i in nCr:
+    if sum(list(i)) == 100:
+        print(*sorted(list(i)), sep='\n')
+        break
