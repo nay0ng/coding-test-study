b55fe82e078d4251391fe0a38b05ffed5c86e0fd
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-21 13:37:29 +0900

    solve: 2442 별 찍기 - 5

--- 0x02/solutions/2442.py
+++ 0x02/solutions/2442.py
@@ -1,4 +1,15 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2442
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+# version 1. 공백 이용
+print(*[" " * (n-i) + "*"*(2*i - 1) for i in range(1, n+1)], sep='\n')
+
+# version 2. f-string 이용
+print(*[f"{'*'*i:>{n}}{'*'*(i-1)}" for i in range(1,n+1)], sep='\n')
+
+# version 3. rjust 이용
+print(*[('*'*i).rjust(n)+'*'*(i-1) for i in range(1, n+1)], sep='\n')
