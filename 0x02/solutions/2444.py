5558d9b42279370404538950584a8c4977d13650
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-22 18:01:59 +0900

    solve: 2444 별 찍기 - 7

--- 0x02/solutions/2444.py
+++ 0x02/solutions/2444.py
@@ -1,4 +1,9 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2444
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+print(*[' '* (n-i) + '*'* (2*i -1) for i in range(1, n+1)], sep='\n')
+print(*[' '* (n-i) + '*'* (2*i -1) for i in range(n-1, 0, -1)], sep='\n')
