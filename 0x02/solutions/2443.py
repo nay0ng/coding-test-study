fb76330b812809904bdd8d06d3765d967341d92f
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-22 17:16:56 +0900

    solve: 2443 별 찍기 - 6

--- 0x02/solutions/2443.py
+++ 0x02/solutions/2443.py
@@ -9,7 +9,7 @@ n = int(input())
 print(*[' ' * i + '*' * (2*(n-i)-1) for i in range(n)], sep='\n')
 
 # version 2. f-string 이용
-# print(*[f"{'*' * (2*i-1):^{2*n-1}}" for i in range(n, 0, -1) ], sep='\n')
+print(*[f"{'*' * (i):>{n}}{'*'*(i-1)}" for i in range(n, 0, -1) ], sep='\n')
 
 # version 3. rjust 이용
-# print(*[('*'*i).rjust(n) + '*' * (i-1) for i in range(n, 0, -1)], sep='\n')
+print(*[('*'*i).rjust(n) + '*' * (i-1) for i in range(n, 0, -1)], sep='\n')
