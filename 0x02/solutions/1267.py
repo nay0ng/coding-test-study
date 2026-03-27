abd5e8965d36f863f92a8bc5052c1ec983d680e3
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-27 10:54:55 +0900

    solve: 1267 핸드폰 요금

--- 0x02/solutions/1267.py
+++ 0x02/solutions/1267.py
@@ -1,4 +1,21 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/1267
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+call_count = int(input())
+call_minute = list(map(int, input().split()))
+
+Y = 0
+M = 0
+
+for i in call_minute:
+    Y += (i // 30 + 1) * 10 
+    M += (i // 60 + 1) * 15
+
+if Y < M:
+    print(f"Y {Y}")
+elif M < Y:
+    print(f"M {M}")
+else:
+    print(f"Y M {Y}")
