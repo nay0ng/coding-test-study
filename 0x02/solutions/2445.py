ddb12986e29a4f5e1e656dcf483d662b9267ef5d
Nayoung Kwak <nayoung32918@gmail.com>
2026-03-27 09:19:59 +0900

    solve: 2445 별 찍기 - 8

--- 0x02/solutions/2445.py
+++ 0x02/solutions/2445.py
@@ -1,4 +1,8 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/2445
 import sys
-input = sys.stdin.readline
+input = lambda:sys.stdin.readline().rstrip()
+n = int(input())
+
+print(*['*' * i + ' ' * 2 * (n-i) + '*' * i for i in range(1, n+1)], sep='\n')
+print(*['*' * i + ' ' * 2 * (n-i) + '*' * i for i in range(n-1, 0, -1)], sep='\n')
