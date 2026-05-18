6086d1da19ca11d0b74ec45d91e61e4905c26754
Nayoung Kwak <nayoung32918@gmail.com>
2026-05-19 07:12:09 +0900

    solve: 10804 카드 역배치

--- 0x02/solutions/10804.py
+++ 0x02/solutions/10804.py
@@ -1,4 +1,22 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/10804
 import sys
-input = sys.stdin.readline
+input = lambda:sys.stdin.readline().rstrip()
+
+num_list = list(range(1, 21))
+
+for _ in range(10):
+    a, b = map(int, input().split())
+
+    # temp = num_list[a-1:b]
+    # temp.reverse()
+    # num_list[a-1:b] = temp
+
+    num_list[a-1:b] = list(reversed(num_list[a-1:b]))
+
+    # for i in list(reversed(num_list[a-1:b])):
+    #     num_list[p] = i
+    #     p += 1
+
+
+print(*num_list)
