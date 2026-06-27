5845c9fc494c0ded2d6eda375eb441f9dc8366c9
Nayoung Kwak <nayoung32918@gmail.com>
2026-06-27 16:11:50 +0900

    solve: 10808 알파벳 개수

--- 0x03/solutions/10808.py
+++ 0x03/solutions/10808.py
@@ -1,4 +1,13 @@
 # Authored by : nayoung918
 # https://www.acmicpc.net/problem/10808
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+word = input()
+cnt_list = [0] * 26
+
+for w in word:
+    idx = ord(w) - ord('a')
+    cnt_list[idx] += 1
+
+print(*cnt_list, sep=' ')
